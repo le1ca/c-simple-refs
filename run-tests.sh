79307a23cf8ea2c62394bafd211606af5b76eea4
Travis Mick <lq@le1.ca>
2015-05-27 20:41:51 -0600

    add script for testing

---
+++ run-tests.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+for f in test*.c; do
+        gcc -Wall -Wpedantic -Werror -o ${f%.c}.out $f
+        echo -n ${f%.c}:\ 
+        valgrind --leak-check=full ./${f%.c}.out 2>&1 | grep "ERROR SUMMARY" | awk '{$1=$2=$3=""; gsub(/^[\t ]+/, "", $0)}1;'
+        rm ./${f%.c}.out
+done

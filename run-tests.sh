295f801c02ac989e75891ba7afb3b156b19eb59a
Travis Mick <lq@le1.ca>
2015-05-29 12:19:48 -0600

    more warnings pls

--- run-tests.sh
+++ run-tests.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 for f in test*.c; do
-        gcc -Wall -Wpedantic -Werror -o ${f%.c}.out $f
+        gcc -Wall -Wpedantic -Wextra -Werror -o ${f%.c}.out $f
         echo -n ${f%.c}:\ 
         valgrind --leak-check=full ./${f%.c}.out 2>&1 | grep "ERROR SUMMARY" | awk '{$1=$2=$3=""; gsub(/^[\t ]+/, "", $0)}1;'
         rm ./${f%.c}.out
